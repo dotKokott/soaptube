50318d0bf0982c0ba05c3c26bf6d564543ad81ed
Christian Kokott <christian.kokott@gmail.com>
2019-11-29 05:52:42 +0100

    removed Links

--- source/server/main.js
+++ source/server/main.js
@@ -1,12 +1,6 @@
 import { Meteor } from 'meteor/meteor';
-import Links from '/imports/api/links';
 import Video from '/imports/api/video';
 
-function insertLink(title, url) {
-  Links.insert({ title, url, createdAt: new Date() });
-}
-
 Meteor.startup(() => {
-  // const vid = new Video('https://www.youtube.com/watch?v=mD66opUGI2o', 'Terence');
-  // vid.download('downloads');
+
 });
